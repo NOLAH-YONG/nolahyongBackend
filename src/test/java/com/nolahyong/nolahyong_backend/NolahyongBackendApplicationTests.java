150fc96e3499ee7bb088f536741949b216e618b2
yunjeong51 <youwjd51@gmail.com>
2025-05-11 20:27:03 +0900

    Initial commit: Set up backend Spring Boot project with dependencies

---
+++ src/test/java/com/nolahyong/nolahyong_backend/NolahyongBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nolahyong.nolahyong_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NolahyongBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
